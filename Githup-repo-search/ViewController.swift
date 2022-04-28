54742972ca3ffae40cd7db5d91c29e18e27b6824
Omar Diab <omar_diab_1995@yahoo.com>
2022-04-29 00:46:45 +0300

    Initial commit.
    Create the project and add table.

--- Githup-repo-search/ViewController.swift
+++ Githup-repo-search/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var reposTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reposTableView.delegate = self
+        reposTableView.dataSource = self
+        reposTableView.allowsSelection = false
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell.init()
+    }
 
 }
 
