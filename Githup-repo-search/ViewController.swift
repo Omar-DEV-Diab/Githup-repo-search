43215792e9a59bfa00bb14a6480ce4fbc59b3eb3
Omar Diab <omar_diab_1995@yahoo.com>
2022-04-30 02:36:20 +0300

    Add table cell and searchBar.
    SearchBar added to the header, delegate implemented and tableViewCell class has been added.

--- Githup-repo-search/ViewController.swift
+++ Githup-repo-search/ViewController.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UISearchBarDelegate {
 
     @IBOutlet weak var reposTableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
+    var array = ["sakhdk","nxz,mn,","uii9934"]
     override func viewDidLoad() {
         super.viewDidLoad()
         reposTableView.delegate = self
         reposTableView.dataSource = self
         reposTableView.allowsSelection = false
+        
+        searchBar.delegate = self
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
@@ -25,9 +29,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell.init()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "repoCell", for: indexPath) as! repoTableViewCell
+        return cell
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard !searchBar.text!.isEmpty else {
+            return
+        }
+        searchQuery()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            return
+        }
+        searchQuery()
     }
 
+    @objc func searchQuery() {
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(searchQuery), object: nil)
+        guard let searchText = searchBar.text else { return }
+        searchService.searchForRepositories(searchText) { (response) in
+            //
+        }
+    }
 }
 
---
+++ Githup-repo-search/repoTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  repoTableViewCell.swift
+//  Githup-repo-search
+//
+//  Created by Omar Diab on 4/29/22.
+//
+
+import UIKit
+
+class repoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+}
